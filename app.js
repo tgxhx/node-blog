8f5aaefb33648020bee71f524c73dec977bffde3
xh <tgxh2020@gmail.com>
2017-11-29 11:16:57 +0800

    express

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.all('*', (req, res, next) => {
 	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
 	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Credentials", true); //可以带cookies
-  res.header("X-Powered-By", '3.2.1')
+  res.header("X-Powered-By", 'Express')
   if (res.xhr) {
     console.log('ajax request.')
   }
